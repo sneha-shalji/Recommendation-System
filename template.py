2df1ac93a7b5e59ef43842590aaf67bea3fc4013
sneha-shalji <mesnehashalji@gmail.com>
2026-03-11 03:07:50 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,53 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format = '[%(asctime)s]: %(message)s:')
+
+project_name = "book_recommeder"
+
+
+list_of_files = [
+    f"{project_name}/__innit__.py",
+    f"{project_name}/components/stage_00_data_ingestion.py",
+    f"{project_name}/components/stage_01_data_validation.py",
+    f"{project_name}/components/stage_02_data_transformation.py",
+    f"{project_name}/components/stage_03_model_trainer.py",
+    f"{project_name}/config/__innit__.py",
+    f"{project_name}/config/sconfiguration.py",
+    f"{project_name}/constant/__innit__.py",
+    f"{project_name}/entity/__innit__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/exception/__innit__.py",
+    f"{project_name}/exception/exception_handler.py",
+    f"{project_name}/logger/__innit__py",
+    f"{project_name}/logger/log.py",
+    f"{project_name}/pipeline/__innit__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/utils/__innit__.py",
+    f"{project_name}/utils/util.py",
+    ".dockerignore",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py"
+]
+
+for filepath in list_of_files:
+    filepath= Path(filepath)
+
+
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file {filename}")
+
+
+    if (not os.path.exists(filename)) or (os.path.getsize(filename) == 0):
+            with open(filepath, 'w') as f:
+                pass
+                logging.info(f"Creating empty file: {filename}")
+
+    else:
+        logging.info(f"{filename} is already created")    
