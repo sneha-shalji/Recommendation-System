d252b091745685df4de25e472cd51e04f0b0acf1
sneha-shalji <mesnehashalji@gmail.com>
2026-03-31 01:21:58 +0530

    data validation added

--- book_recommeder/config/configuration.py
+++ book_recommeder/config/configuration.py
@@ -3,7 +3,7 @@ import sys
 from book_recommeder.logger.log import logging
 from book_recommeder.utils.util import read_yaml_files
 from book_recommeder.exception.exception_handler import AppException
-from book_recommeder.entity.config_entity import DataIngestionConfig
+from book_recommeder.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from book_recommeder.constant.__init__ import *
 
 class AppConfiguration:
@@ -33,6 +33,34 @@ class AppConfiguration:
         
         except Exception as e:
             raise AppException(e,sys) from e
+        
+    def get_data_validation_config(self) -> DataValidationConfig:
+        try:
+            data_validation_config = self.configs_info['data_validation_config']
+            data_ingestion_config =self.configs_info['data_ingestion_config']
+            dataset_dir = data_ingestion_config['dataset_dir']
+            artifacts_dir = self.configs_info['artifacts_config']['artifacts_dir']
+            books_csv_file = data_validation_config['books_csv_file']
+            ratings_csv_file = data_validation_config['ratings_csv_file']
+
+            books_csv_file_dir = os.path.join(artifacts_dir,dataset_dir,data_ingestion_config['ingested_dir'],books_csv_file)
+            ratings_csv_file = os.path.join(artifacts_dir,dataset_dir,data_ingestion_config['ingested_dir'],ratings_csv_file)
+            clean_data_path = os.path.join(artifacts_dir,dataset_dir,data_validation_config['clean_data_dir'])
+            serialized_objects_dir = os.path.join(artifacts_dir,data_validation_config['serialized_objects_dir'])
+
+            response = DataValidationConfig(
+                clean_data_dir= clean_data_path,
+                books_csv_file= books_csv_file_dir,
+                ratings_csv_file= ratings_csv_file,
+                serialized_objects_dir=serialized_objects_dir
+            )
+
+            logging.info(f"Data Validation config: {response}")
+            return response
+
+        except Exception as e:
+            raise AppException(e,sys) from e
+ 
         
 
 
--- book_recommeder/pipeline/training_pipeline.py
+++ book_recommeder/pipeline/training_pipeline.py
@@ -1,9 +1,11 @@
 from book_recommeder.components.stage_00_data_ingestion import DataIngestion
+from book_recommeder.components.stage_01_data_validation import DataValidation
 
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
+        self.data_validation = DataValidation()
 
 
     def start_training_pipeline(self):
@@ -13,3 +15,5 @@ class TrainingPipeline:
         
         """
         self.data_ingestion.initiate_data_ingestion()
+        self.data_validation.initiate_data_validation()
+
